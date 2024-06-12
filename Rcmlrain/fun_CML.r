153ebc8398e274bb4cdd29ace0241290133f8a40
Martin Fencl <martin.fencl@cvut.cz>
2024-06-12 10:43:27 +0200

    fix bug in get_ITU_pars
    fix bug in get_ITU_pars when checking frequency range

--- Rcmlrain/fun_CML.r
+++ Rcmlrain/fun_CML.r
@@ -705,7 +705,7 @@ get_ITU_pars <- function(freq, pol, conv = T, digits = 3){
     if(length(pol) != length(which(is.na(match(pol, c("V", "H")))==F))){
         stop("Polarizaton has to be character vector with only V or H symbol for vert. resp. horizon. polarization")
     }
-    if(freq < 1 || freq > 1000){stop("Frequency is out of ITU rec. range")}
+    if(min(freq) < 1 || max(freq) > 1000){stop("Frequency is out of ITU rec. range")}
     
     #load ITU data (can be later extended to 1 GHz)
     Fr <- c(1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,120,150,200,300,400,500,600,700,800,900,1000)
